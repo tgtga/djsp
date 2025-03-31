7c317e0891e1205cd6288ce081943f7e4f1303b1
tgtga <dracodrago1330@gmail.com>
2025-03-31 22:06:56 +0000

    add a bash script to automatically generate a plot for a given number

---
+++ data.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+  echo "lol kill -9 saved"
+  exit 1
+fi
+
+{
+  echo '$DATA <<END';
+  stdbuf -oL ./djsp.rb -s -- $1 \
+    | perl -lne 'print if /step/' \
+    | perl -pe 's/.+ step (\d+) ~2\*\*(\d+)/$1 $2/';
+  echo 'END';
+
+  cat <<HERE
+set term svg
+set output '$1.svg'
+
+set key noautotitle
+set key title '$1'
+
+set nonlinear y via log10(y) inverse 10**y
+set ytics log
+
+f(x) = 10**ceil(log10(x))
+stats \$DATA nooutput
+set xrange [-10:STATS_max_x + 10]
+set yrange [1:f(STATS_max_y)]
+
+plot \$DATA w l
+HERE
+} | gnuplot
