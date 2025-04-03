ccdc9dadbe7e6d83d513c47c7210691c581e8c78
tgtga <dracodrago1330@gmail.com>
2025-04-03 15:26:01 +0000

    closes #5: stop using the tetration inverse function and calculate the inverse of the left-associative tower with a nested log

--- data.rb
+++ data.rb
@@ -2,7 +2,8 @@
 
 require "open3"
 
-TETRATION = eval ENV["tetration"] || "false"
+BASE   = eval ENV["base"]   || ENV["BASE"]   || "1.5"
+LOGLOG = eval ENV["loglog"] || ENV["LOGLOG"] || "false"
 
 if STDIN.tty? # no data waiting in stdin
   case ARGV.length
@@ -25,43 +26,8 @@ end
 
 data = data.filter_map {|line| line.match(/.+ step (\d+) ~2\*\*(\d+)/)&.[](1..)&.map(&:to_i) }
 
-if TETRATION
-  Open3.popen2(*%W[gp -q]) do |i, o, _|
-    i.puts <<~HERE
-      read("kneser.gp");
-      quietmode = 1;
-      init(3/2);
-    HERE
-    21.times { o.gets } # get rid of kneser.gp preamble
-
-    data.map! do |a|
-      w, v = a
-
-      i.puts "print(slog(#{v}));"
-      
-      r = Float o.gets
-
-      a = [w, r]
-    end
-  end
-end
-
 so = data.map {|a| a.join " " }.join("\n") + "\n"
 File.write "#{N}.dat", so
-
-driver =
-  if TETRATION
-    "set yrange [2:ceil(STATS_max_y)]"
-  else
-    <<~HERE
-      set nonlinear y via log10(y) inverse 10**y
-      set ytics log
-
-      f(x) = 10**ceil(log10(x))
-      set yrange [1:f(STATS_max_y)]
-    HERE
-  end
-
 
 Open3.capture2 *%W[gnuplot], stdin_data: <<~HERE
   $DATA <<END
@@ -76,7 +42,12 @@ Open3.capture2 *%W[gnuplot], stdin_data: <<~HERE
 
   stats $DATA nooutput
 
-  #{driver}
+  set ytics log
+  IL = 1/log(#{BASE})
+  set nonlinear y via #{LOGLOG ? "log(log(y) * IL) * IL inverse #{BASE}**#{BASE}**y" : "log(y) * IL inverse #{BASE}**y"}
+
+  f(x) = 10**ceil(log10(x))
+  set yrange [#{LOGLOG ? 10 : 2}:f(STATS_max_y)]
   set xrange [-10:STATS_max_x + 10]
 
   plot $DATA w l
